bd892e308e1a141dd1921dbc5e36693a5f3744c0
ottebokma <60065432+ottebokma@users.noreply.github.com>
2021-01-26 10:45:29 +0100

    src_updt

--- src/svm.py
+++ src/svm.py
@@ -5,20 +5,20 @@ from sklearn import svm
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
 
-
-#choosing comparison data (y value)
-#cmp = "ptsd_6mo"
-
 #loading data
 data = pd.read_csv("data/journal_pone_0169490_s010.csv")
 
 """ all data
-"patientnum", "marshall", "rotterdam", "ct_intracranial_final", "skullfx", "skullbasefx", "facialfx", "edh_final", "sdh_final", "sah_final", "contusion_final", "midlineshift_final", "cisterncomp_final", "mr_result", "gose_overallscore3m", "gose_overallscore6m", "ptsd_6mo", "wais_psi_composite_6mo", "cvltshortdelaycuedrecallstandardscore_6mo", "cvltlongdelaycuedrecallstandardscore_6mo", "rs4680", "rs6277", "rs3219119", "rs11604671", "rs4938016", "rs1800497"
+"patientnum", "marshall", "rotterdam", "ct_intracranial_final", "skullfx", "skullbasefx",
+"facialfx", "edh_final", "sdh_final", "sah_final", "contusion_final", "midlineshift_final", "cisterncomp_final", "mr_result",
+"gose_overallscore3m", "gose_overallscore6m", "ptsd_6mo", "wais_psi_composite_6mo", "cvltshortdelaycuedrecallstandardscore_6mo",
+"cvltlongdelaycuedrecallstandardscore_6mo", "rs4680", "rs6277", "rs3219119", "rs11604671", "rs4938016", "rs1800497"
 """
 
-data = data[["marshall", "rotterdam", "ct_intracranial_final", "skullfx", "skullbasefx", "facialfx", "edh_final", "sdh_final", "sah_final", "contusion_final", "midlineshift_final", "cisterncomp_final", "rs4680", "rs6277", "rs3219119", "rs11604671", "rs4938016", "rs1800497", "gose_overallscore6m"]]
+data = data[["marshall", "rs4680", "rs6277", "rs3219119", "rs11604671", "rs4938016", "rs1800497", "ct_intracranial_final", "skullfx", "skullbasefx",
+"facialfx", "edh_final", "sdh_final", "sah_final", "contusion_final", "midlineshift_final", "cisterncomp_final"]]
 
-predict = "gose_overallscore6m"
+predict = "marshall"
 
 data = data.dropna(how="any")
 
@@ -31,13 +31,13 @@ print (data.shape)
 
 sum_acc = 0
 
-iter = 3000
+iter = 5
 
 for n in range(iter):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 
 
-    clf = svm.SVC(kernel="rbf", degree = 2)
+    clf = svm.SVC(kernel="poly", degree = 3)
     clf.fit(x_train, y_train)
 
     y_pred = clf.predict(x_test)
@@ -47,20 +47,19 @@ for n in range(iter):
 
     sum_acc += acc
 
-print(sum_acc)
+#print(sum_acc)
 
 avg = sum_acc / iter
 print (avg)
 
-#print (y_pred)
-#print (y_test)
+print (y_pred)
+print (y_test)
 
-"""
-clf2 = KNeighborsClassifier(n_neighbors = 5)
+
+clf2 = KNeighborsClassifier(n_neighbors = 4)
 clf2.fit(x_train, y_train)
 
 y_pred2 = clf2.predict(x_test)
 
 acc2 = metrics.accuracy_score(y_test, y_pred2)
-print (acc2)
-"""
+print ("KNN: " + str(acc2))
